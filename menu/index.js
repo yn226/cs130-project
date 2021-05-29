d6f0aa022196fa90184ac6bbc2a7c0d2c052672d
Yuhka Niki <yuhkaniki@Yuhkas-MacBook-Pro.local>
2021-05-28 20:38:13 -0700

    Updating project

--- menu/index.js
+++ menu/index.js
@@ -8,3 +8,7 @@ const menu = [
     'MenuItems/Mocha.jpg',
     'MenuItems/Tea.jpg'
 ];
+const audioFile = 'https://p.scdn.co/mp3-preview/0b87de031d085eb01ba57cb76dad26b54695f374?cid=9697a3a271d24deea38f8b7fbfa0e13c';
+const audioPlayer = AudioPlayer('.player', audioFile);
+
+initScreen();
